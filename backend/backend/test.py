af93c691146de30babd821c7b954911f64f49266
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-03-13 22:10:37 +0200

    added postgresql support + security beggining

---
+++ backend/backend/test.py
@@ -0,0 +1,9 @@
+# from dotenv import load_dotenv
+# import os
+# from pathlib import Path
+
+# env_path = Path('..') / '.env'
+# load_dotenv(dotenv_path=env_path)
+
+# secret_key = os.getenv("SECRET_KEY")
+# print(secret_key)

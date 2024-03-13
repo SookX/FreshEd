02d92ebc1cc45d336f547374faaf28d095305eb0
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-13 22:17:29 +0200

    Colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-
+      {/* <div className="pink test">PINK</div>
+      <div className="blue test">BLUE</div> */}
+      {/* <div className="green test">GREEN</div> */}
+      <div className="green2 test">GREEN 2</div>
+      <div className="purple test">PURPLE</div>
+      {/* <div className="orange test">ORANGE</div> */}
     </>
   )
 }

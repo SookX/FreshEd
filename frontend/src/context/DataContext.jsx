84258b5154acd814407ce3cfafc7908be450210d
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-14 16:02:39 +0200

    Getting The Tests

--- frontend/src/context/DataContext.jsx
+++ frontend/src/context/DataContext.jsx
@@ -1,9 +1,62 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const DataContext = createContext({})
 
 const DataProvider = ({ children }) => {
+    // TESTS
+    const [tests, setTests] = useState([])
+
+    useEffect(() => {
+        const fetching = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/school/api/test/')
+
+                console.log(response)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        fetching()
+    }, [])
+
+    // EXERCISE
+    const [exercises, setExercises] = useState([])
+
+    useEffect(() => {
+        const fetching = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/school/api/exercise/')
+
+                console.log(response)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        fetching()
+    }, [])
+
+    // EXERCISE
+    const [answers, setAnswers] = useState([])
+
+    useEffect(() => {
+        const fetching = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/school/api/answer/')
+
+                console.log(response)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        fetching()
+    }, [])
+
+
     // NAVIGATION
     const navigate = useNavigate()
 

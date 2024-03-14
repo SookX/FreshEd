c99b6e9957efd9d8034962c385dbfde12e801610
anastassow <127148449+anastassow@users.noreply.github.com>
2024-03-14 14:05:35 +0200

    working view for the test questions and answers

---
+++ backend/school/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Test, Answers, Exercise
+
+class testSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ['id', 'title']
+
+class answersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answers
+        fields = ['id', 'name']
+
+class exerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['id', 'name']
--- backend/school/views.py
+++ backend/school/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Exercise, Test, Answers
+from .serializers import testSerializer, answersSerializer, exerciseSerializer
 # Create your views here.
+
+@api_view(['GET'])
+def test(request, *args, **kwargs):
+    test = Test.objects.all()
+    serializer = testSerializer(test, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def exercise(request, *args, **kwargs):
+    exercise = Exercise.objects.all()
+    serializer = exerciseSerializer(exercise, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def answer(request, *args, **kwargs):
+    answer = Answers.objects.all()
+    serializer = answersSerializer(answer, many = True)
+    return Response(serializer.data)

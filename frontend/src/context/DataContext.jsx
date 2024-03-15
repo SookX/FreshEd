4e51b212740e076b958b7f89ebed679c50a93767
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-15 18:28:42 +0200

    fmhjlhkfhgp;iughjhi

--- frontend/src/context/DataContext.jsx
+++ frontend/src/context/DataContext.jsx
@@ -24,9 +24,9 @@ const DataProvider = ({ children }) => {
 
     useEffect(() => {
         const fetching = async () => {
-            const response = await axios.get('http://127.0.0.1:8000/school/api/combined/')
+            const response = await axios.get('http://127.0.0.1:8000/school/api/testView/')
 
-            setTests(response.data.tests)
+            setTests(response.data[0].tests)
         }
 
         fetching()

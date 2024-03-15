10d156ca8aa0c751624faba766e7413680ff958b
anastassow <127148449+anastassow@users.noreply.github.com>
2024-03-15 02:49:33 +0200

    set up to work again

--- backend/school/urls.py
+++ backend/school/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import createClass, createTeacher
+from .views import createClass, createTeacher, combined_data
 from . import views
 
 urlpatterns = [
     path('createClass/', createClass, name = 'createClass'),
     path('createTeacher/', createTeacher, name = 'createTeacher'),
-    path('addSchool/', views.addSchool)
+    path('addSchool/', views.addSchool),
+    path('api/combined/', views.combined_data, name='combined_api'),
 ]

d94567a058a5cdac0ea748889192851dd815fa18
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-15 23:57:49 +0200

    Home

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -4,12 +4,5 @@ from . import views
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-<<<<<<< Updated upstream
     path('isUser/', views.isUser),
-    path('info/', views.getUserCredentials),
-    path('save/', views.saveChanges),
-=======
-    path('isUser/', views.isUser)
-
->>>>>>> Stashed changes
 ]

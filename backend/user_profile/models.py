a4c8f827d9ae5291796a72629fc372ea98812124
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-03-15 19:51:24 +0200

    validators

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from .validators import image_extension_validator
+
 
 def server_icon_upload_path(instance, filename):
     return f"user/{instance.id}/profile_picutre/{filename}"
@@ -37,7 +39,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255)
     role = models.BooleanField(default=0)
     is_staff = models.BooleanField(default=False)
-    profile_picture = models.ImageField(upload_to=server_icon_upload_path, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=server_icon_upload_path, blank=True, null=True, validators=[image_extension_validator])
 
     objects = UserAccountManager()
     USERNAME_FIELD = 'email'

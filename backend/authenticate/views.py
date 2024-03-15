8ca3140b2134366038e5e125f3b66100ba3286a6
SookX <vagrivas08@gmail.com>
2024-03-16 00:08:25 +0200

    login view

--- backend/authenticate/views.py
+++ backend/authenticate/views.py
@@ -35,7 +35,20 @@ def getGrades(student_id):
 
     return grades_list
 
-
+@api_view(['POST', 'GET'])
+def login(request, *args, **kwargs):
+    body = request.body
+    data = json.loads(body)
+    email = data['email']
+    password = data['password']
+    try:
+        user = UserAccount.objects.get(email=email)
+    except UserAccount.DoesNotExist:
+        return Response(data={"message": "This account doesn't exist."}, status=404)
+    if check_password(password, user.password):
+        return Response(data = {"id": str(user.id)}, status=200)
+    else:
+        return Response(data = {"message": "Wrong password."}, status=404)
 @api_view(['POST', 'GET'])
 def isUser(request, *args, **kwargs):
     if request.method == "POST":
